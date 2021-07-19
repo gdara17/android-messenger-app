74946e4d0a8be8aefe838abc6b62e568a27efbc7
gdara17 <gdara17@freeuni.edu.ge>
2021-07-20 02:22:52 +0400

    initial layouts added

--- Messenger App/app/src/main/java/ge/gdara17/messengerapp/MainActivity.kt
+++ Messenger App/app/src/main/java/ge/gdara17/messengerapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package ge.gdara17.messengerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello World!")
     }
 }
